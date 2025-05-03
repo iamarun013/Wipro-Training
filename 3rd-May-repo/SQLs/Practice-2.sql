8f9c0cab8746f40a30fc0dac34dd8447a8202162
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-03 12:52:27 +0530

    Insertion into students table

---
+++ 3rd-May-repo/SQLs/Practice-2.sql
@@ -0,0 +1,6 @@
+INSERT INTO Students
+Values
+(1, 'Arunava' ,'Saha', 23, 'Computer Science', 100),
+(2, 'Meghoma' ,'Saha', 21, 'Engish', 80),
+(3, 'Arnab' ,'Pal', 23, 'Computer Science', 950),
+(4, 'Polty' ,'Singh', 20, 'IT', 42);
