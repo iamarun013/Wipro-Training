359afbe53c6f89f8b63c93950c728518ba85f9b7
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-03 13:00:07 +0530

    Table_Creation

---
+++ 3rd-May-repo/SQLs/Assignment-1.sql
@@ -0,0 +1,9 @@
+-- Table Creation Employees
+CREATE TABLE Employeess (
+    Employee_ID INT PRIMARY KEY,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2),
+    Hire_DATE DATE
+);
