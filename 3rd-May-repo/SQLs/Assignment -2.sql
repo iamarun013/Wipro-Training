b526422e3bbeb89049842b6ecadea0e4088f47a8
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-03 13:06:14 +0530

    Insert Operation

--- 3rd-May-repo/SQLs/Assignment -2.sql
+++ 3rd-May-repo/SQLs/Assignment -2.sql
@@ -1,1 +1,4 @@
-INSERT INTO Emp
+INSERT INTO Employeess (Employee_ID, First_name, Last_name, Department, Salary, Hire_DATE)
+VALUES
+(101, 'Chanda', 'Biswas', 'CSE', 99.00, '2023-06-01'),
+(102, 'Aman', 'Hussain', 'Commerce', 86.00, '2023-07-15');
