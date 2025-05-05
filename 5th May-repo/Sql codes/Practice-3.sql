f0c85d3758a760c7da6a6cf8009a0e6d25123b64
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-05 15:17:51 +0530

    Ranking functions

--- 5th May-repo/Sql codes/Practice-3.sql
+++ 5th May-repo/Sql codes/Practice-3.sql
@@ -5,7 +5,39 @@ WHERE Department = 'IT';
 
 COMMIT;
 
---BEGIN TRANSACTION
---DELETE FROM Students WHERE Marks <60;
-----Realized it's a mistake
---RollBack;
+BEGIN TRANSACTION
+DELETE FROM Students WHERE Marks <60;
+--Realized it's a mistake
+RollBack;
+
+BEGIN TRANSACTION;
+UPDATE Employees
+SET Salary = Salary + 1000
+WHERE EmpID = 1;
+
+SAVE TRANSACTION IncreaseJohn;
+
+UPDATE Employees
+SET Salary = Salary + 1000
+WHERE EmpID = 2;
+
+--ops rollback second increment only
+ROLLBACK TRANSACTION IncreaseJohn;
+COMMIT;
+
+----
+Partition BY (Group-Wise Analysis)
+SELECT
+Name, Department, Salary,
+AVG(Salary) OVER(Partition BY Department) AS DeptAvgSalary
+FROM Employees;
+
+--Ranking Functions
+Row_Number()
+SELECT
+Name,
+Department,
+Salary,
+Row_Number() OVER(Partition by Department Order by Salaey DESX) AS Row_Number
+
+FROM Employees;
