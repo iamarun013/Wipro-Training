9c852a625eda375a02b0bfbfdee35c0ba3aa4222
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-07 12:31:42 +0530

    increased the list and now trying to delete some elements

--- 7th-May-repo/codes/Practice-1.cs
+++ 7th-May-repo/codes/Practice-1.cs
@@ -9,17 +9,24 @@ class Program
         List.Add("APPLE");
         List.Add(42);
         List.Add(3.14);
+        List.Add("grape");
+        List.Add(433);
+        List.Add(5.14);
+
 
         foreach (var item in List)
         {
             Console.WriteLine(item);
         }
-         Console.WriteLine("Press enter to continue");
+        Console.WriteLine("Press enter to continue");
 
         Console.Read();
 
         // inserting an element at position 1
         List.Insert(1, "inserted");
+
+        List.Remove("grape");
+        List.RemoveAt(2);
         foreach (var item in List)
         {
             Console.WriteLine(item);
