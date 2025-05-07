b581ed6f8058682ba10a5ff912a684f13927784c
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-07 12:15:13 +0530

    Inserting in ArrayList added

--- 7th-May-repo/codes/Practice-1.cs
+++ 7th-May-repo/codes/Practice-1.cs
@@ -9,12 +9,23 @@ class Program
         List.Add("APPLE");
         List.Add(42);
         List.Add(3.14);
-            
-        foreach ( var item in List)    
-            {
-                Console.WriteLine(item);
-            }
-            Console.Read();
-            }
 
+        foreach (var item in List)
+        {
+            Console.WriteLine(item);
+        }
+         Console.WriteLine("Press enter to continue");
+
+        Console.Read();
+
+        // inserting an element at position 1
+        List.Insert(1, "inserted");
+        foreach (var item in List)
+        {
+            Console.WriteLine(item);
+        }
+        Console.Read();
+
+
+    }
 }
