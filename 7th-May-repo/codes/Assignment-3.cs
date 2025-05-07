db55539d5c6c2b25e6b53aeda1cc88bf1ee3ef35
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-07 09:54:01 +0530

    Duplicate words find

---
+++ 7th-May-repo/codes/Assignment-3.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        String line = "Hi my name is Arunava your name is ?";
+        string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            if (!dic.ContainsKey(word))
+            {
+                dic.Add(word, 0);
+            }
+            dic[word]++;
+        }
+
+
+    }
+}
