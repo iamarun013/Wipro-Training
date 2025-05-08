7316ce2034f601e7a4423db5b45bed510c16afb5
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-08 12:09:24 +0530

    1st time get set trying in the assignment

--- 8th- May-repo/codes/Assignment-1.cs
+++ 8th- May-repo/codes/Assignment-1.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Collections.Generic;
 
+class Employee{
+    public int ID{get; set;}
+    public string names{get;, set;}
+}
+
 class Program
 {
     static void Main()
     {
-        List<string> employee_names = new List<string>();
-        employee_names.Add("Arun");
-        employee_names.Add("Arnab");
-        employee_names.Add("Abir");
-        employee_names.Add("Meghoma");
-        employee_names.Add("bhairav");
-        employee_names.Add("Poltu");
-
+        List<Employee> employee_names = new List<Employee>();
+        {
+        new employee_names{ID=1, names="Arun" },
+        new employee_names{ID=2, names="Arnab"},
+        new employee_names{ID=3, names="Abir"},
+        new employee_names{ID=4, names="Meghoma"},
+        new employee_names{ID=5, names="Bhairav"},
+        new employee_names{ID=6, names="Poltu"}
+        
+        };
+        employee_names.Add(new Employee(ID=7, names="God"))
+        
         employee_names.Remove("Poltu");
         if (employee_names.Contains("abir"))
 
