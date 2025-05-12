dcccd54bc6d32172d3798c5973bca210d8e61fd6
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-12 12:48:18 +0530

    Abstract class

---
+++ 12th May - repo/codes/Abstract-Practice-3.cs
@@ -0,0 +1,24 @@
+abstract class Shape{
+    public abstract void Draw();// Abstract method
+
+    public void Display()
+    {
+        Console.WriteLine("This is a shape");
+    }
+
+}
+
+class Circle: Shape
+
+public override void Draw()
+{
+    Console.WriteLine("Drawing a circle");
+}
+
+class Program{
+    static void Main()
+    {
+        Circle circle = new Circle();
+        circle.Draw();
+    }
+}
