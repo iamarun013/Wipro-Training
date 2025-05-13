f55478a1f616684917799c2189a32a906957ce6c
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-13 12:34:06 +0530

    using add as a delegate without using delegate keyword  using func

--- 13th-May-repo/codes/delegate-exp-Practice-4.cs
+++ 13th-May-repo/codes/delegate-exp-Practice-4.cs
@@ -5,18 +5,29 @@ class SimpleDelegate
     public delegate int SomeOperation(int i, int j);
 }
 
-class Program
+class Function
 {
     static int Sum(int x, int y)
     {
         return x + y;
     }
+    // Assign the Sum method to a Func delegate
+    //first two ints are parameters to the functtion a and the last one is return type, we  here are not just using delegate keyword, we are using func to use this aas like a delegate
+
 
     static void Main(string[] args)
     {
-        // Use the delegate type from SimpleDelegate
-        SimpleDelegate.SomeOperation add = Sum;
+        Func<int, int, int> add = Sum;
+
+        // Invoke the delegate
         int result = add(10, 10);
+
         Console.WriteLine(result);
     }
 }
+//     
+//         // Use the delegate type from SimpleDelegate
+//         SimpleDelegate.SomeOperation add = Sum;
+//         int result = add(10, 10);
+//         Console.WriteLine(result);
+//
