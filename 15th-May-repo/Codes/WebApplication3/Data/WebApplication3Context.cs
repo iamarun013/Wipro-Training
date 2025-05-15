5803155368e0f242eb6e9ef269eca9d35be0a16f
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-15 17:04:46 +0530

    WebApplication-Employee

---
+++ 15th-May-repo/Codes/WebApplication3/Data/WebApplication3Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class WebApplication3Context : DbContext
+    {
+        public WebApplication3Context (DbContextOptions<WebApplication3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication3.Models.Student> Student { get; set; } = default!;
+        public DbSet<WebApplication3.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ 15th-May-repo/Codes/WebApplication3/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+
+        public string EmployeeDepartment { get; set; }
+    }
+}
