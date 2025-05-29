7a59c547df749941ac9f105247d308315a25eb78
Arunava Saha <asahamultipurpose13@gmail.com>
2025-05-29 16:28:00 +0530

    updating the project

---
+++ 29th-May-repo/Codes/EmployeeManagementSystem/EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime DateofJoining { get; set; }
+
+        public decimal Salary {  get; set; }
+
+    }
+}
